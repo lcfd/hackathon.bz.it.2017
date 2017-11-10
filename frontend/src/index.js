5e68bca25116807f5ee1f61ae6ff55e63a66be43
lucafedri <luca.fedrizzi@belka.us>
2017-11-10 17:02:18 +0100

    Adding redux to fontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import './css/global.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { rootReducer } from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function logger({ getState }) {
+  return next => action => {
+    console.info('dispatching', action);
+    const result = next(action);
+    console.log('state after', getState());
+    return result;
+  };
+}
+
+const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
+const store = createStoreWithMiddleware(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
