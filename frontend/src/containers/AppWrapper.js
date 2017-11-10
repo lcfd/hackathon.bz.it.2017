b681d11290015ffba2ad1f60a1cc3488c9f689b2
lucafedri <luca.fedrizzi@belka.us>
2017-11-11 00:21:13 +0100

    bottom bar with icons

--- frontend/src/containers/AppWrapper.js
+++ frontend/src/containers/AppWrapper.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { BottomMainMenu } from '../components/BottomMainMenu';
 import { Link } from 'react-router-dom';
 
+import dashboard from '../assets/img/dashboard.svg';
+import investments from '../assets/img/investments.svg';
+import profile from '../assets/img/profile.svg';
+
 class AppWrapper extends Component {
   render() {
     return (
@@ -9,13 +13,28 @@ class AppWrapper extends Component {
         {this.props.children}
         <BottomMainMenu>
           <div className="bottomMainMenu__element">
-            <Link to="/">Home</Link>
+            <Link to="/">
+              <p>
+                <img src={dashboard} alt="" />
+              </p>
+              <p>Dashboard</p>
+            </Link>
           </div>
           <div className="bottomMainMenu__element">
-            <Link to="/investments">Investments</Link>
+            <Link to="/investments">
+              <p>
+                <img src={investments} alt="" />
+              </p>
+              <p>Investments</p>
+            </Link>
           </div>
           <div className="bottomMainMenu__element">
-            <Link to="/profile">Profilo</Link>
+            <Link to="/profile">
+              <p>
+                <img src={profile} alt="" />
+              </p>
+              <p>Investments</p>
+            </Link>
           </div>
         </BottomMainMenu>
       </div>
