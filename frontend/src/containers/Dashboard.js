f8108bfefc81f3d6c4f962ef71f34a36fe85eae0
lucafedri <luca.fedrizzi@belka.us>
2017-11-11 08:01:10 +0100

    Deploy on Netifly

--- frontend/src/containers/Dashboard.js
+++ frontend/src/containers/Dashboard.js
@@ -107,186 +107,215 @@ class DashboardContainer extends Component {
           main="Your investments"
           sub={`${(parseInt(this.props.user.investedValue3Month) * 1.42).toFixed(0)}€`}
         />
-        <Box>
-          <img className="dashboard__box-salvadanaio-img" src={salvadanaio} alt="" />
-          <p className="box__weekly-stats">MONTHLY STATS</p>
-          <p className="box__weekly-stats-amount">
-            € <strong>{parseInt(this.state.aggregation).toFixed(2)}</strong>
-          </p>
-          <p className="box__investment-this-week">Investment this month</p>
-          <hr style={{ marginBottom: 0 }} />
-          <div className="row">
-            <div className="col-6">
-              <p className="box__text-number-in-split">
-                € <strong>{parseInt(this.state.aggregationPastMonth).toFixed(2)}</strong>
+        <div className="row">
+          <div className="col-12 col-md-3">askdjflakjsdflksjdflajksdf</div>
+          <div className="col-12 col-md-9">
+            <Box>
+              <img className="dashboard__box-salvadanaio-img" src={salvadanaio} alt="" />
+              <p className="box__weekly-stats">MONTHLY STATS</p>
+              <p className="box__weekly-stats-amount">
+                € <strong>{parseInt(this.state.aggregation).toFixed(2)}</strong>
               </p>
-              <p className="box__text-under-number">Previous month</p>
-              <div className="box__weekly-vertical-line" />
-            </div>
+              <p className="box__investment-this-week">Investment this month</p>
+              <hr style={{ marginBottom: 0 }} />
+              <div className="row">
+                <div className="col-6">
+                  <p className="box__text-number-in-split">
+                    € <strong>{parseInt(this.state.aggregationPastMonth).toFixed(2)}</strong>
+                  </p>
+                  <p className="box__text-under-number">Previous month</p>
+                  <div className="box__weekly-vertical-line" />
+                </div>
 
-            <div className="col-6">
-              <p className="box__text-number-in-split">
-                € <strong>{parseInt(this.state.monthlyAverage).toFixed(2)}</strong>
-              </p>
-              <p className="box__text-under-number">Monthly average</p>
-            </div>
+                <div className="col-6">
+                  <p className="box__text-number-in-split">
+                    € <strong>{parseInt(this.state.monthlyAverage).toFixed(2)}</strong>
+                  </p>
+                  <p className="box__text-under-number">Monthly average</p>
+                </div>
+              </div>
+            </Box>
+            <p className="dashboard__your-active-investmets">YOUR ACTIVE INVESTMENTS</p>
+
+            {/* Investments */}
+
+            <Box>
+              {!this.state.fuelIsFlipped ? (
+                <div>
+                  <div className="box__investment-content">
+                    <p className="box__investment-title">
+                      <img src={fuel} alt="" /> Fuel
+                    </p>
+                    <p className="box__investment-text">When I buy fuel I want to invest the 10% of what I paid.</p>
+                    <div className="box__investment-perc-oval">{this.props.user.fuelPercentage}%</div>
+                  </div>
+                  <div className="box__left-arrow-action" data-name="fuel" onClick={e => this.handleClickSetting(e)}>
+                    <img className="" data-name="fuel" src={edit} alt="" />
+                  </div>
+                  <div className="clear" />
+                </div>
+              ) : (
+                <div>
+                  <div className="box__investment-content">
+                    <p className="box__investment-title">Invest {this.props.user.fuelPercentage} %</p>
+                    <input
+                      type="range"
+                      min="1"
+                      max="30"
+                      step="1"
+                      data-name="fuel"
+                      value={parseInt(this.props.user.fuelPercentage)}
+                      onChange={e => this.handleSetPercentage(e)}
+                    />
+                  </div>
+                  <div className="box__left-arrow-action" data-name="fuel" onClick={e => this.handleClickSetting(e)}>
+                    <img className="" data-name="fuel" src={close} alt="" />
+                  </div>
+                  <div className="clear" />
+                </div>
+              )}
+            </Box>
+
+            <Box>
+              {!this.state.fastFoodIsFlipped ? (
+                <div>
+                  <div className="box__investment-content">
+                    <p className="box__investment-title">
+                      <img src={hambuger} alt="" /> Fast food
+                    </p>
+                    <p className="box__investment-text">
+                      When I buy a meal from a fast food I want to invest the 10% of what I paid.
+                    </p>
+                    <div className="box__investment-perc-oval">{this.props.user.fastFoodPercentage}%</div>
+                  </div>
+                  <div
+                    className="box__left-arrow-action"
+                    data-name="fastFood"
+                    onClick={e => this.handleClickSetting(e)}
+                  >
+                    <img className="" data-name="fastFood" src={edit} alt="" />
+                  </div>
+                  <div className="clear" />
+                </div>
+              ) : (
+                <div>
+                  <div className="box__investment-content">
+                    <p className="box__investment-title">Invest {this.props.user.fastFoodPercentage} %</p>
+                    <input
+                      type="range"
+                      min="1"
+                      max="30"
+                      step="1"
+                      data-name="fastFood"
+                      value={parseInt(this.props.user.fastFoodPercentage)}
+                      onChange={e => this.handleSetPercentage(e)}
+                    />
+                  </div>
+                  <div
+                    className="box__left-arrow-action"
+                    data-name="fastFood"
+                    onClick={e => this.handleClickSetting(e)}
+                  >
+                    <img className="" data-name="fastFood" src={close} alt="" />
+                  </div>
+                  <div className="clear" />
+                </div>
+              )}
+            </Box>
+
+            <Box>
+              {!this.state.ecommerceIsFlipped ? (
+                <div>
+                  <div className="box__investment-content">
+                    <p className="box__investment-title">
+                      <img src={ecommerce} alt="" /> Ecommerce
+                    </p>
+                    <p className="box__investment-text">
+                      When I buy products from an ecommerce I want to invest the 10% of what I paid.
+                    </p>
+                    <div className="box__investment-perc-oval">{this.props.user.ecommercePercentage}%</div>
+                  </div>
+                  <div
+                    className="box__left-arrow-action"
+                    data-name="ecommerce"
+                    onClick={e => this.handleClickSetting(e)}
+                  >
+                    <img className="" data-name="ecommerce" src={edit} alt="" />
+                  </div>
+                  <div className="clear" />{' '}
+                </div>
+              ) : (
+                <div>
+                  <div className="box__investment-content">
+                    <p className="box__investment-title">Invest {this.props.user.ecommercePercentage} %</p>
+                    <input
+                      type="range"
+                      min="1"
+                      max="30"
+                      step="1"
+                      data-name="ecommerce"
+                      value={parseInt(this.props.user.ecommercePercentage)}
+                      onChange={e => this.handleSetPercentage(e)}
+                    />
+                  </div>
+                  <div
+                    className="box__left-arrow-action"
+                    data-name="ecommerce"
+                    onClick={e => this.handleClickSetting(e)}
+                  >
+                    <img className="" data-name="ecommerce" src={close} alt="" />
+                  </div>
+                  <div className="clear" />
+                </div>
+              )}
+            </Box>
+
+            <Box>
+              {!this.state.transportIsFlipped ? (
+                <div>
+                  <div className="box__investment-content">
+                    <p className="box__investment-title">
+                      <img src={plane} style={{ width: '24px' }} alt="" />Transport
+                    </p>
+                    <p className="box__investment-text">When I travel I want to invest the 7% of what I paid.</p>
+                    <div className="box__investment-perc-oval">{this.props.user.transportPercentage}%</div>
+                  </div>
+                  <div
+                    className="box__left-arrow-action"
+                    data-name="transport"
+                    onClick={e => this.handleClickSetting(e)}
+                  >
+                    <img className="" data-name="transport" src={edit} alt="" />
+                  </div>
+                  <div className="clear" />
+                </div>
+              ) : (
+                <div>
+                  <div className="box__investment-content">
+                    <p className="box__investment-title">Invest {this.props.user.transportPercentage} %</p>
+                    <input
+                      type="range"
+                      min="1"
+                      max="30"
+                      step="1"
+                      data-name="transport"
+                      value={parseInt(this.props.user.transportPercentage)}
+                      onChange={e => this.handleSetPercentage(e)}
+                    />
+                  </div>
+                  <div
+                    className="box__left-arrow-action"
+                    data-name="transport"
+                    onClick={e => this.handleClickSetting(e)}
+                  >
+                    <img className="" data-name="transport" src={close} alt="" />
+                  </div>
+                  <div className="clear" />
+                </div>
+              )}
+            </Box>
           </div>
-        </Box>
-        <p className="dashboard__your-active-investmets">YOUR ACTIVE INVESTMENTS</p>
-
-        {/* Investments */}
-
-        <Box>
-          {!this.state.fuelIsFlipped ? (
-            <div>
-              <div className="box__investment-content">
-                <p className="box__investment-title">
-                  <img src={fuel} alt="" /> Fuel
-                </p>
-                <p className="box__investment-text">When I buy fuel I want to invest the 10% of what I paid.</p>
-                <div className="box__investment-perc-oval">{this.props.user.fuelPercentage}%</div>
-              </div>
-              <div className="box__left-arrow-action" data-name="fuel" onClick={e => this.handleClickSetting(e)}>
-                <img className="" data-name="fuel" src={edit} alt="" />
-              </div>
-              <div className="clear" />
-            </div>
-          ) : (
-            <div>
-              <div className="box__investment-content">
-                <p className="box__investment-title">Invest {this.props.user.fuelPercentage} %</p>
-                <input
-                  type="range"
-                  min="1"
-                  max="30"
-                  step="1"
-                  data-name="fuel"
-                  value={parseInt(this.props.user.fuelPercentage)}
-                  onChange={e => this.handleSetPercentage(e)}
-                />
-              </div>
-              <div className="box__left-arrow-action" data-name="fuel" onClick={e => this.handleClickSetting(e)}>
-                <img className="" data-name="fuel" src={close} alt="" />
-              </div>
-              <div className="clear" />
-            </div>
-          )}
-        </Box>
-
-        <Box>
-          {!this.state.fastFoodIsFlipped ? (
-            <div>
-              <div className="box__investment-content">
-                <p className="box__investment-title">
-                  <img src={hambuger} alt="" /> Fast food
-                </p>
-                <p className="box__investment-text">
-                  When I buy a meal from a fast food I want to invest the 10% of what I paid.
-                </p>
-                <div className="box__investment-perc-oval">{this.props.user.fastFoodPercentage}%</div>
-              </div>
-              <div className="box__left-arrow-action" data-name="fastFood" onClick={e => this.handleClickSetting(e)}>
-                <img className="" data-name="fastFood" src={edit} alt="" />
-              </div>
-              <div className="clear" />
-            </div>
-          ) : (
-            <div>
-              <div className="box__investment-content">
-                <p className="box__investment-title">Invest {this.props.user.fastFoodPercentage} %</p>
-                <input
-                  type="range"
-                  min="1"
-                  max="30"
-                  step="1"
-                  data-name="fastFood"
-                  value={parseInt(this.props.user.fastFoodPercentage)}
-                  onChange={e => this.handleSetPercentage(e)}
-                />
-              </div>
-              <div className="box__left-arrow-action" data-name="fastFood" onClick={e => this.handleClickSetting(e)}>
-                <img className="" data-name="fastFood" src={close} alt="" />
-              </div>
-              <div className="clear" />
-            </div>
-          )}
-        </Box>
-
-        <Box>
-          {!this.state.ecommerceIsFlipped ? (
-            <div>
-              <div className="box__investment-content">
-                <p className="box__investment-title">
-                  <img src={ecommerce} alt="" /> Ecommerce
-                </p>
-                <p className="box__investment-text">
-                  When I buy products from an ecommerce I want to invest the 10% of what I paid.
-                </p>
-                <div className="box__investment-perc-oval">{this.props.user.ecommercePercentage}%</div>
-              </div>
-              <div className="box__left-arrow-action" data-name="ecommerce" onClick={e => this.handleClickSetting(e)}>
-                <img className="" data-name="ecommerce" src={edit} alt="" />
-              </div>
-              <div className="clear" />{' '}
-            </div>
-          ) : (
-            <div>
-              <div className="box__investment-content">
-                <p className="box__investment-title">Invest {this.props.user.ecommercePercentage} %</p>
-                <input
-                  type="range"
-                  min="1"
-                  max="30"
-                  step="1"
-                  data-name="ecommerce"
-                  value={parseInt(this.props.user.ecommercePercentage)}
-                  onChange={e => this.handleSetPercentage(e)}
-                />
-              </div>
-              <div className="box__left-arrow-action" data-name="ecommerce" onClick={e => this.handleClickSetting(e)}>
-                <img className="" data-name="ecommerce" src={close} alt="" />
-              </div>
-              <div className="clear" />
-            </div>
-          )}
-        </Box>
-
-        <Box>
-          {!this.state.transportIsFlipped ? (
-            <div>
-              <div className="box__investment-content">
-                <p className="box__investment-title">
-                  <img src={plane} style={{ width: '24px' }} alt="" />Transport
-                </p>
-                <p className="box__investment-text">When I travel I want to invest the 7% of what I paid.</p>
-                <div className="box__investment-perc-oval">{this.props.user.transportPercentage}%</div>
-              </div>
-              <div className="box__left-arrow-action" data-name="transport" onClick={e => this.handleClickSetting(e)}>
-                <img className="" data-name="transport" src={edit} alt="" />
-              </div>
-              <div className="clear" />
-            </div>
-          ) : (
-            <div>
-              <div className="box__investment-content">
-                <p className="box__investment-title">Invest {this.props.user.transportPercentage} %</p>
-                <input
-                  type="range"
-                  min="1"
-                  max="30"
-                  step="1"
-                  data-name="transport"
-                  value={parseInt(this.props.user.transportPercentage)}
-                  onChange={e => this.handleSetPercentage(e)}
-                />
-              </div>
-              <div className="box__left-arrow-action" data-name="transport" onClick={e => this.handleClickSetting(e)}>
-                <img className="" data-name="transport" src={close} alt="" />
-              </div>
-              <div className="clear" />
-            </div>
-          )}
-        </Box>
+        </div>
       </AppWrapper>
     );
   }
