9a79ee9aaf55a092c3059d005fc20cf498463717
lucafedri <luca.fedrizzi@belka.us>
2017-11-11 07:44:35 +0100

    Fixing and hide bottom bar on dsk

--- frontend/src/containers/Profile.js
+++ frontend/src/containers/Profile.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import AppWrapper from '../containers/AppWrapper';
 import { ProfileHeader } from '../components/ProfileHeader';
 
+import { dashboardMonthAggregationNoPerc } from './utils/dataAggregations';
+
 /**
  * css
  */
@@ -14,7 +16,53 @@ import peterDolonPic from '../assets/img/peterDolonPic.png';
 import totalCake from '../assets/img/totalCake.png';
 
 class ProfileContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      totalExpenses: {
+        finalSum: 0,
+        totEcommerce: 0,
+        totFastFood: 0,
+        totFuel: 0,
+        totTransport: 0
+      }
+    };
+  }
+
+  componentDidMount() {
+    let aggregation = dashboardMonthAggregationNoPerc(this.props, 2);
+    let aggregationPastMonth = dashboardMonthAggregationNoPerc(this.props, 3);
+    let aggregationPastPastMonth = dashboardMonthAggregationNoPerc(this.props, 4);
+    let totalExpenses =
+      parseInt(aggregation.finalSum) +
+      parseInt(aggregationPastMonth.finalSum) +
+      parseInt(aggregationPastPastMonth.finalSum);
+    this.setState({
+      totalExpenses: {
+        finalSum: totalExpenses,
+        totEcommerce:
+          parseInt(aggregation.totEcommerce) +
+          parseInt(aggregationPastMonth.totEcommerce) +
+          parseInt(aggregationPastPastMonth.totEcommerce),
+        totFastFood:
+          parseInt(aggregation.totFastFood) +
+          parseInt(aggregationPastMonth.totFastFood) +
+          parseInt(aggregationPastPastMonth.totFastFood),
+        totFuel:
+          parseInt(aggregation.totFuel) +
+          parseInt(aggregationPastMonth.totFuel) +
+          parseInt(aggregationPastPastMonth.totFuel),
+        totTransport:
+          parseInt(aggregation.totTransport) +
+          parseInt(aggregationPastMonth.totTransport) +
+          parseInt(aggregationPastPastMonth.totTransport)
+      }
+    });
+  }
+
   render() {
+    console.log(this.state.totalExpenses);
+
     return (
       <AppWrapper class="profile container">
         <ProfileHeader userPic={peterDolonPic} user={{ name: 'Peter Dolon' }} />
@@ -26,7 +74,7 @@ class ProfileContainer extends Component {
           <div className="col-4 left">SEPT 2017</div>
         </div>
         <p className="profile__centered-title">TOTAL EXPENSES</p>
-        <p className="profile__centered-money">€ 500.00</p>
+        <p className="profile__centered-money">€ {this.state.totalExpenses.finalSum}</p>
         <img className="profile__cake-chart" src={totalCake} alt="" />
       </AppWrapper>
     );
--- frontend/src/containers/utils/dataAggregations.js
+++ frontend/src/containers/utils/dataAggregations.js
@@ -65,3 +65,60 @@ export function dashboardMonthAggregation(props, subtractMonth) {
   console.log(finalSum);
   return finalSum;
 }
+
+export function dashboardMonthAggregationNoPerc(props, subtractMonth) {
+  let totFuel = 0;
+  let totEcommerce = 0;
+  let totFastFood = 0;
+  let totTransport = 0;
+
+  const currentMonth = moment().format('M') - subtractMonth;
+  /**
+   * Fuel
+   */
+  let filteredFuel = _.filter(databank.transactions, o => {
+    const theMoth = moment(o.details.completed).format('M');
+    return o.details.description == 'Filling Station' && theMoth == currentMonth;
+  });
+  filteredFuel.map(o => {
+    totFuel += o.details.value * -1;
+  });
+  console.log(totFuel);
+  /**
+   * Ecommerce
+   */
+  let filteredEcommerce = _.filter(databank.transactions, o => {
+    const theMoth = moment(o.details.completed).format('M');
+    return o.details.description == 'Shopping' && theMoth == currentMonth;
+  });
+  filteredEcommerce.map(o => {
+    totEcommerce += o.details.value * -1;
+  });
+  console.log(totEcommerce);
+  /**
+   * Fast food
+   */
+  let filteredFastFood = _.filter(databank.transactions, o => {
+    const theMoth = moment(o.details.completed).format('M');
+    return o.details.description == 'Restaurants' && theMoth == currentMonth;
+  });
+  filteredFastFood.map(o => {
+    totFastFood += o.details.value * -1;
+  });
+  console.log(totFastFood);
+  /**
+   * Tranports
+   */
+  let filteredTransports = _.filter(databank.transactions, o => {
+    const theMoth = moment(o.details.completed).format('M');
+    return o.details.description == 'train ticket' && theMoth == currentMonth;
+  });
+  filteredTransports.map(o => {
+    totTransport += o.details.value * -1;
+  });
+  console.log(totTransport);
+
+  const finalSum = (totEcommerce + totFastFood + totFuel + totTransport).toFixed(2);
+  console.log(finalSum);
+  return { finalSum, totEcommerce, totFastFood, totFuel, totTransport };
+}
